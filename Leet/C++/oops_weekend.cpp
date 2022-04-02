c64017959d7aca4e53bcf6454b52d515780a75da
モヒット <100509432+LaFinBuddha@users.noreply.github.com>
2022-04-02 18:47:39 +0530

    Update oops_weekend.cpp

--- Leet/C++/oops_weekend.cpp
+++ Leet/C++/oops_weekend.cpp
@@ -8,15 +8,14 @@ int ptsm(int a, int b){
 }
 
 void maxpt(int a,int b){
-    int *p11=&a;
-    int *p22=&b;
+    
+    int *p11=&a,*p22=&b;
+    
     if(*p11>*p22)
         cout<<*p22<<" is smaller";
-    else if(*p11==*p22){
+    else if(*p11==*p22)
         cout<<*p11<<" is equal to "<<*p22;
-    }
-    else{
+    else
         cout<<*p11<<" is smaller";
-    }
 }
 
